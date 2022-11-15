27ac9ea5241504a4954be8b85f5ef2a96b5e0640
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-15 19:18:00 +0700

    Ввод пароля

---
+++ Задача1/Program.cs
@@ -0,0 +1,36 @@
+﻿// . В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+string message = "hi";
+string password = "secret";
+
+Console.WriteLine("Введите пароль: ");
+string a = Console.ReadLine();
+
+if (a == password)
+{
+   Console.WriteLine($"{message}");
+}
+
+else 
+{
+    Console.WriteLine("Введите пароль ещё раз: ");
+    a = Console.ReadLine();
+}
+
+if ( a == password)
+{
+   Console.WriteLine($"{message}");
+}
+
+else 
+{
+    Console.WriteLine("Введите пароль в последний раз: ");
+    a = Console.ReadLine();
+}
+if ( a == password)
+{
+   Console.WriteLine($"{message}");
+}
+else 
+{
+    Console.WriteLine("Пароль неверный!");
+}
